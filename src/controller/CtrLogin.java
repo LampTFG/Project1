f7bab4eb92a3a025c1a72a06fe5061b2e607838f
Felipe Mammoli <fmammoli@gmail.com>
2013-04-17 23:05:15 -0300

    ctrl login now check md5 from user password

--- src/controller/CtrLogin.java
+++ src/controller/CtrLogin.java
@@ -8,29 +8,19 @@ import model.User;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
 import dataBase.DBConn2;
 
 public class CtrLogin {
-	/*public boolean validaUser(Context context, User u){
-		DBConn2 conn = new DBConn2(context);
-		String[] allColumns = { "username", "password"};
-		SQLiteDatabase db = conn.getDatabase();
-		Cursor cursor = db.query("User", allColumns, "username = '"+u.getLogin()+"' and password='"+u.getPass()+"'", null, null, null, null);
-		boolean answer = false;
-    	if (cursor.moveToFirst())
-    		answer = true;
-    	cursor.close();
-    	conn.Close(db);
-    	return answer;
-    }*/
+	
 	public boolean validaUser(Context context, User u){
 		try {
 			User regUser = new CustomerRequester().execute(u.getLogin()).get();
-			System.out.println("RegUser pass "+regUser.getPass());
-			System.out.println("User pass "+u.getPass()+" _MD5: "+Functions.md5(u.getPass()));
-			if(regUser.getPass() == Functions.md5(u.getPass()))
+			if(regUser.getPass().equals( Functions.md5(u.getPass()))){
+				Log.d("CtrLogin", "user is valid!");
 				return true;
+			}
 			else
 				return false;
 		} catch (InterruptedException e) {
