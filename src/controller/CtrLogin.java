90594615bfd525bfadaa81e3359a8033857a3609
Felipe Mammoli <fmammoli@gmail.com>
2013-04-17 23:14:51 -0300

    infos about the login flow

--- src/controller/CtrLogin.java
+++ src/controller/CtrLogin.java
@@ -12,10 +12,20 @@ import android.util.Log;
 
 import dataBase.DBConn2;
 
+/*
+ * User enter email and pass
+ * CustomerRequester makes an call searching for customer_by_email
+ * XMLParser2 parses the response to find the customer id
+ * CustomerRequester call for customer_by_id
+ * XMLParser2 parses all user information needed to build a user
+ * CtrlLogin check if the entered password is equal the requested user.
+ */
+
 public class CtrLogin {
 	
 	public boolean validaUser(Context context, User u){
 		try {
+			//It request an user by its email/login
 			User regUser = new CustomerRequester().execute(u.getLogin()).get();
 			if(regUser.getPass().equals( Functions.md5(u.getPass()))){
 				Log.d("CtrLogin", "user is valid!");
